b0412c10b4d849bab44ee1e9b5b4bacf6aaa1121
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2023-09-21 12:22:23 -0300

    v1 com rotas privadas

--- src/main/java/sp3/com/swagger/docswagger/TesteswaggerApplication.java
+++ src/main/java/sp3/com/swagger/docswagger/TesteswaggerApplication.java
@@ -2,10 +2,19 @@ package sp3.com.swagger.docswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import sp3.com.swagger.docswagger.filtros.FiltroDeTokensJWT;
 
 @SpringBootApplication
 public class TesteswaggerApplication {
-
+    @Bean
+    public FilterRegistrationBean<FiltroDeTokensJWT> filterJwt() {
+        FilterRegistrationBean<FiltroDeTokensJWT> filterRB = new FilterRegistrationBean<FiltroDeTokensJWT>();
+        filterRB.setFilter(new FiltroDeTokensJWT());
+        filterRB.addUrlPatterns("/v1/api/produtos/*", "/auth/usuarios");
+        return filterRB;
+    }
     public static void main(String[] args) {
         SpringApplication.run(TesteswaggerApplication.class, args);
     }
